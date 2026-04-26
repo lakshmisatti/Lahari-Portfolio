e0a94b4cfb8cb1ea357979244ef0ff1196d052a5
Lakshmi Lahari Satti <lakshmisatti@arizona.edu>
2026-04-25 21:19:32 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').Config} */
 const nextConfig = {
-  output: 'export', // This tells Next.js to generate static HTML/CSS/JS
-  images: {
-    unoptimized: true, // Necessary for static exports
-  },
+  output: 'export',
+  basePath: '/Lahari-Portfolio', // MUST match your GitHub repo name exactly
+  assetPrefix: '/Lahari-Portfolio/', 
+  images: { unoptimized: true },
 };
-
 export default nextConfig;
